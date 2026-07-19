1a926f2968c59b796b1cd68edf6f2acce561d3c0
abhijith-account <abhijithp.student@gmail.com>
2026-07-19 18:30:15 +0530

    15th one

--- scripts/check_coverage.sh
+++ scripts/check_coverage.sh
@@ -7,14 +7,49 @@ mkdir -p coverage_report
 
 gcovr \
     --root . \
-    --object-directory tests/build/ \
+    --object-directory tests/build \
+    --filter src/ \
     --exclude-unreachable-branches \
     --exclude-throw-branches \
-    --filter src/ \
     --fail-under-line 100 \
     --fail-under-branch 100 \
+    --txt coverage_report/coverage.txt \
     --html-details coverage_report/index.html \
-    --print-summary
+    --sonarqube coverage_report/sonarqube.xml \
+    --print-summary \
+    --verbose
 
-echo "SUCCESS: 100% Line and Branch Coverage Verified. HTML Report generated."
-exit 0
+echo
+echo "================ UNCOVERED LINES ================"
+gcovr \
+    --root . \
+    --object-directory tests/build \
+    --filter src/ \
+    --exclude-unreachable-branches \
+    --exclude-throw-branches \
+    --txt-metric line \
+    --txt
+
+echo
+echo "=============== UNCOVERED BRANCHES =============="
+gcovr \
+    --root . \
+    --object-directory tests/build \
+    --filter src/ \
+    --exclude-unreachable-branches \
+    --exclude-throw-branches \
+    --txt-metric branch \
+    --txt
+
+gcovr \
+    --root . \
+    --object-directory tests/build \
+    --filter src/ \
+    --decisions \
+    --html-details coverage_report/index.html
+
+echo
+echo "Detailed HTML report:"
+echo "coverage_report/index.html"
+
+echo "SUCCESS: Coverage verification complete."
